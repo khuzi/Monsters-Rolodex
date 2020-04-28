7500dd645c9c8345c2f070127f33672d801b171f
khuzaima yaseen <khuzaimayaseen@gmail.com>
2020-04-28 13:39:44 -0700

    Updates

---
+++ precache-manifest.81a7a9eae0f0a553aa956cef79c36695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244ef7cfc6306b5486649baadc263357",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "0ff252089db89278bf2b",
+    "url": "/Monsters-Rolodex/static/css/main.726eb714.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff252089db89278bf2b",
+    "url": "/Monsters-Rolodex/static/js/main.bb1f5439.chunk.js"
+  },
+  {
+    "revision": "bb1db29ffdd7a4ded20c",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.f852ebba.js"
+  }
+]);
